809150755b84a55b486c49e58494e8856ff13764
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-10-08 07:07:40 -0400

    commit passive buzzer serial

--- pySerial/passive_buzzer.py
+++ pySerial/passive_buzzer.py
@@ -8,10 +8,13 @@ timeout = 1
 
 #initialize the serial port
 ser = serial.Serial(arduino_port, baud_rate, timeout = timeout)
+time.sleep(2) # wait for arduino to come online
 
 def read_passive():
     while True:
-        return ser.readline().decode('utf-8').strip()
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').strip()
+            print(f'Received from Arduino: {line}')
 
 def main():
     try:
