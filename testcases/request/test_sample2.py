994583411a4412cb87fa5f505514f8118e111795
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2025-01-14 17:28:43 -0500

    use the global varible to store the response data

--- testcases/request/test_sample2.py
+++ testcases/request/test_sample2.py
@@ -1,9 +1,19 @@
-# 2. get the interface of the tag
+# 2. get the interface of the tag 获取已创建的标签接口
 # Interface association (接口关联): refers to the process of linking or connecting multiple APIs
 import requests as req
 import jsonpath as jp
 
+'''
+rule of jsonpath expression:
+$ root node
+. child node (sub-node) -> $.access_token
+.. recursion achieve child node
+[] represent get the value of list, start at 0 -> [{id:0}, {id:1}, {id:2}]
+'''
+
+
 class TestApi:
+    access_token = ""
     def test_get_token(self):
         url = 'https://api.weixin.qq.com/cgi-bin/token'
         datas = {
@@ -13,10 +23,23 @@ class TestApi:
         }
 
         res = req.get(url, params=datas)
-        print(res.json())
+        result = res.json()
+        print(result)
+
+        # get access token
+        value = jp.jsonpath(result, "$.access_token")
+        # print(value)
+        TestApi.access_token = value[0]
 
     def test_select_flag(self):
         url = "http://api.weixin.qq.com/cgi-bin/tags/get"
         datas = {
-            "access_token": ""
+            "access_token": TestApi.access_token
         }
+        res = req.get(url, params=datas)
+        print(res.json())
+
+if __name__ == '__main__':
+    t = TestApi()
+    t.test_get_token()
+    t.test_select_flag()
