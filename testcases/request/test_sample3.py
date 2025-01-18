ebdfefc2a046a43b6e5d72ce8e049b9133eba4d4
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2025-01-17 22:41:37 -0500

    create flag interface

---
+++ testcases/request/test_sample3.py
@@ -0,0 +1,48 @@
+# 3. create the interface of the tag
+
+import random
+import requests as req
+import jsonpath as jp
+import json
+
+class TestApi:
+    access_token = ""
+
+    def test_get_token(self):
+        url = "https://api.weixin.qq.com/cgi-bin/token"
+        datas = {
+            "grant_type": "client_credential",
+            "appid": "wx74a8627810cfa308",
+            "secret": "e40a02f9d79a8097df497e6aaf93ab80"
+        }
+
+        res = req.get(url, params=datas)
+        result = res.json()
+        print(result)
+
+        # get access token
+        value = jp.jsonpath(result, "$.access_token")
+        # print(value)
+        TestApi.access_token = value[0]
+
+    def test_select_flag(self):
+        url = "https://api.weixin.qq.com/cgi-bin/tags/get"
+        datas = {
+            "access_token": TestApi.access_token
+        }
+        res = req.get(url, params=datas)
+        print(res.json())
+
+    def test_create_flag(self):
+        url = "https://api.weixin.qq.com/cgi-bin/tags/create?access_token="+TestApi.access_token
+        datas = {"tag":{"name":"广东"+str(random.randint(10000,99999))}}
+        res = req.post(url, json=datas)
+        # print(res.json())
+        rs = json.loads(json.dumps(res.json()).replace("\\\\","\\"))
+        print(rs)
+
+if __name__ == '__main__':
+    test = TestApi()
+    test.test_get_token()
+    test.test_select_flag()
+    test.test_create_flag()
