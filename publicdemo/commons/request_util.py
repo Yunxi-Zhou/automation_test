34066761034e91cf67d8d5605fcb880df4f212ea
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2025-02-03 23:18:22 -0500

    request util commons

--- publicdemo/commons/request_util.py
+++ publicdemo/commons/request_util.py
@@ -1,11 +1,13 @@
 import requests
 
 class RequestUtil:
-    sess = requests.session()
+   sess = requests.session()
     
-    # common send requests
-    def all_send_request(self, **kwargs):
-       res = RequestUtil.sess.request(**kwargs)
-       print("method: "+kwargs["method"])
-       print(res.text)
-       return res
+   # common send requests
+   # 原理： 请求头： keep-alive
+   # Connection: keep-alive 保持活跃
+   def all_send_request(self, **kwargs):
+      res = RequestUtil.sess.request(**kwargs)
+   #    print("method: "+kwargs["method"])
+   #    print(res.text)
+      return res
